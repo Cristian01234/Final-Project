db950bb8c4c41527391e2b3ad5c2fe58966cad3a
40751 <cristyardy2@gmail.com>
2021-08-09 19:36:06 +0300

    Initial commit

---
+++ src/main/java/com/sda/onlineAuction/dto/ProductDto.java
@@ -0,0 +1,24 @@
+package com.sda.onlineAuction.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+@Getter
+@Setter
+@ToString
+public class ProductDto {
+
+    private String name;
+    private String description;
+    private String startBiddingPrice;
+    private String category;
+    private String endDateTime;
+
+
+
+
+
+}
